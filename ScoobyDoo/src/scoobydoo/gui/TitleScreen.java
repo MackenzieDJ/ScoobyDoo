140e560c70762c4694addb5b4b547db7719c93b6
Earthcomputer <burtonjae@hotmail.co.uk>
2016-10-10 10:39:42 +0100

    Add image disappear example

--- ScoobyDoo/src/scoobydoo/gui/TitleScreen.java
+++ ScoobyDoo/src/scoobydoo/gui/TitleScreen.java
@@ -2,22 +2,36 @@ package scoobydoo.gui;
 
 import java.awt.Color;
 
+import scoobydoo.engine.gui.ImageDisplay;
 import scoobydoo.engine.gui.Screen;
 import scoobydoo.engine.input.Images;
 
 public class TitleScreen extends Screen {
 
+	private static final int RODAWAY_DISAPPEAR_DELAY = 100;
+
+	private int ticksExisted = 0;
+	private ImageDisplay rodaway;
+
 	@Override
 	public void layout() {
 		addButton(width / 2 - 150 / 2, height / 2 - 50 / 2, 150, 50, "Hello World", "hello");
 		addLabel(50, 50, 100, 100, "Hello Scooby", Color.GREEN);
-		addImage(0, 0, 50, 50, Images.test);
+		rodaway = addImage(0, 0, 50, 50, Images.test);
 	}
 
 	@Override
 	public void onButtonPressed(String buttonId) {
 		if ("hello".equals(buttonId)) {
 			System.out.println("Hello World");
+		}
+	}
+
+	@Override
+	public void updateTick() {
+		ticksExisted++;
+		if (ticksExisted > RODAWAY_DISAPPEAR_DELAY) {
+			removeComponent(rodaway);
 		}
 	}
 
