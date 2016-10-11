a83ccc2e1a7caa5df78754d26f348ef0699af5f0
Earthcomputer <burtonjae@hotmail.co.uk>
2016-10-11 11:42:28 +0100

    Add central GameLogic class

---
+++ ScoobyDoo/src/scoobydoo/main/GameLogic.java
@@ -0,0 +1,52 @@
+package scoobydoo.main;
+
+import java.awt.Image;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+import scoobydoo.resources.Images;
+
+public class GameLogic {
+
+	private GameLogic() {
+	}
+	
+	private static List<GangMember> aliveMembers = new ArrayList<GangMember>();
+
+	public static List<GangMember> getAliveMembers() {
+		return aliveMembers;
+	}
+
+	public static enum GangMember {
+		SCOOBY(Images.Scooby), SHAGGY(Images.Shaggy), VELMA(Images.velma), DAPHNE(Images.Daf), FRED(Images.FredB);
+
+		private Image image;
+
+		private GangMember(Image image) {
+			this.image = image;
+		}
+
+		public Image getImage() {
+			return image;
+		}
+
+		public GangMember randomMember(Random rand, List<GangMember> excluding) {
+			if (excluding.containsAll(Arrays.asList(values()))) {
+				throw new IllegalArgumentException("No gang members to pick from");
+			}
+
+			GangMember chosen;
+			do {
+				chosen = randomMember(rand);
+			} while (excluding.contains(chosen));
+			return chosen;
+		}
+
+		public GangMember randomMember(Random rand) {
+			return values()[rand.nextInt(values().length)];
+		}
+	}
+
+}
