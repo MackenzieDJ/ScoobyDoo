d0e25061f57abf47d43b939d4d6ef6fa9baf92d0
Earthcomputer <burtonjae@hotmail.co.uk>
2016-10-11 13:23:47 +0100

    Add sound engine

---
+++ ScoobyDoo/src/scoobydoo/engine/sound/DummySoundPlayer.java
@@ -0,0 +1,15 @@
+package scoobydoo.engine.sound;
+
+class DummySoundPlayer implements ISoundPlayer {
+
+	@Override
+	public void play(String name, Runnable onStopped) {
+		// nop
+	}
+
+	@Override
+	public void close() {
+		// nop
+	}
+
+}
